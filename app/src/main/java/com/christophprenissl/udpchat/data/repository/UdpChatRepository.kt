d0a5ea951515f9e0ed2d96ca21c688d784d257aa
Christoh Prenissl <christophprenissl@gmail.com>
2024-08-04 17:41:58 +0200

    Update UdpChatRepository to setup addresses and ports and also receiving of messages

--- app/src/main/java/com/christophprenissl/udpchat/data/repository/UdpChatRepository.kt
+++ app/src/main/java/com/christophprenissl/udpchat/data/repository/UdpChatRepository.kt
@@ -1,19 +1,47 @@
 package com.christophprenissl.udpchat.data.repository
 
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.withContext
 import java.net.DatagramPacket
 import java.net.DatagramSocket
 import java.net.InetAddress
 
 class UdpChatRepository {
+    private var destinationIpAddress: String = "10.0.2.2"
+    private var sendPort: Int = 12345
+    private var receivePort: Int = 54321
 
-    suspend fun sendMessage(message: String, ipAddress: String, port: Int) {
+    fun setup(destinationIpAddress: String, sendPort: Int, receivePort: Int) {
+        this.destinationIpAddress = destinationIpAddress
+        this.sendPort = sendPort
+        this.receivePort = receivePort
+    }
+
+    fun receiveMessages() = flow {
+        val socket = DatagramSocket(receivePort)
+        val receiveData = ByteArray(1024)
+        val receivePacket = DatagramPacket(receiveData, receiveData.size)
+        while (true) {
+            socket.receive(receivePacket)
+            val message = String(receivePacket.data, 0, receivePacket.length)
+            emit(message)
+        }
+    }
+        .flowOn(Dispatchers.IO)
+
+    suspend fun sendMessage(message: String) {
         withContext(Dispatchers.IO) {
             val socket = DatagramSocket()
             val sendData = message.toByteArray()
             val sendPacket =
-                DatagramPacket(sendData, sendData.size, InetAddress.getByName(ipAddress), port)
+                DatagramPacket(
+                    sendData,
+                    sendData.size,
+                    InetAddress.getByName(destinationIpAddress),
+                    sendPort
+                )
             socket.send(sendPacket)
             socket.close()
         }
